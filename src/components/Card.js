31f25b7fb1117e49b088afba9866ed4e8ad34c20
Jeffrey Pratt <jeffrey@jeffreypratt.org>
2025-05-29 18:06:02 -0700

    Some more style tweaks.

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,6 +2,9 @@ import React from "react";
 
 const IMG_BASE = `${process.env.PUBLIC_URL || ''}/cards/`;
 
+console.log('IMG_BASE:', IMG_BASE);
+console.log('process.env.PUBLIC_URL:', process.env.PUBLIC_URL);
+
 let getCardUrl = (cardName) => {
     // Convert card format from "Ad", "Td", etc. to "AD.svg", "10D.svg", etc.
     const rank = cardName.charAt(0);
@@ -14,16 +17,23 @@ let getCardUrl = (cardName) => {
 };
 
 const Card = (props) => {
+    console.log('Card props:', props);
+    const backUrl = IMG_BASE + "RED_BACK.svg";
+    console.log('Card back URL:', backUrl);
+    
     if (props.card && props.revealed) {
+        const cardUrl = getCardUrl(props.card);
+        console.log('Card URL:', cardUrl);
         return (
             <img
                 className="card"
-                src={getCardUrl(props.card)}
+                src={cardUrl}
                 alt="Playing card"
             />
         );
     } else {
-        return <img className="card" src={IMG_BASE + "RED_BACK.svg"} alt="Card back" />;
+        console.log('Showing card back');
+        return <img className="card" src={backUrl} alt="Card back" />;
     }
 };
 
