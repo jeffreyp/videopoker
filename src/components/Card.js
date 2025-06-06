524b21f65bd6d4317b46365bf91d6fe112f95e53
Jeffrey Pratt <jeffrey@jeffreypratt.org>
2025-06-05 17:01:32 -0700

    More refactoring by Claude.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,40 +1,40 @@
-import React from "react";
+import React, { useMemo } from "react";
 
 const IMG_BASE = `${process.env.PUBLIC_URL || ''}/cards/`;
 
-console.log('IMG_BASE:', IMG_BASE);
-console.log('process.env.PUBLIC_URL:', process.env.PUBLIC_URL);
-
-let getCardUrl = (cardName) => {
-    // Convert card format from "Ad", "Td", etc. to "AD.svg", "10D.svg", etc.
+const getCardUrl = (cardName) => {
     const rank = cardName.charAt(0);
     const suit = cardName.charAt(1).toUpperCase();
-    
-    // Convert T to 10 for the filename
     const fileName = rank === 'T' ? '10' + suit : rank.toUpperCase() + suit;
-    
     return `${IMG_BASE}${fileName}.svg`;
 };
 
-const Card = (props) => {
-    console.log('Card props:', props);
-    const backUrl = IMG_BASE + "RED_BACK.svg";
-    console.log('Card back URL:', backUrl);
+const Card = ({ card, revealed, id }) => {
+    const backUrl = useMemo(() => IMG_BASE + "RED_BACK.svg", []);
     
-    if (props.card && props.revealed) {
-        const cardUrl = getCardUrl(props.card);
-        console.log('Card URL:', cardUrl);
+    const cardContent = useMemo(() => {
+        if (card && revealed) {
+            const cardUrl = getCardUrl(card);
+            return (
+                <img
+                    className="card"
+                    src={cardUrl}
+                    alt={`Playing card ${card}`}
+                    loading="lazy"
+                />
+            );
+        }
         return (
-            <img
-                className="card"
-                src={cardUrl}
-                alt="Playing card"
+            <img 
+                className="card" 
+                src={backUrl} 
+                alt="Card back"
+                loading="lazy"
             />
         );
-    } else {
-        console.log('Showing card back');
-        return <img className="card" src={backUrl} alt="Card back" />;
-    }
+    }, [card, revealed, backUrl]);
+
+    return cardContent;
 };
 
 export default React.memo(Card);
